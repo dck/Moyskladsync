9d674aa19b4117d769fd11352016145504949ced
Denis Usanov <denis@usanov.info>
2017-10-21 17:10:02 +0300

    Fix imports

--- lib/moyskladsync.rb
+++ lib/moyskladsync.rb
@@ -3,8 +3,7 @@ require 'json'
 require 'logger'
 require 'google_drive'
 
-require_relative 'moyskladsync/clients/moysklad'
-require_relative 'moyskladsync/clients/gdrive'
+require_relative 'moyskladsync/clients'
 require_relative 'moyskladsync/product'
 require_relative 'moyskladsync/logger'
 
