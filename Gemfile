83267b32f0c3a9029d014c9a597120ebab0dbe2a
Denis Usanov <denis@usanov.info>
2018-02-25 21:04:00 +0300

    Parse brewery, name and style

--- Gemfile
+++ Gemfile
@@ -1,4 +1,9 @@
 source 'https://rubygems.org'
 
 gem 'google_drive'
+gem 'rake'
 gem 'rest-client'
+
+group :test do
+  gem 'minitest'
+end
--- lib/moyskladsync/product.rb
+++ lib/moyskladsync/product.rb
@@ -1,8 +1,29 @@
 module Moyskladsync
-  Product = KeywordStruct.new(
-    :id,
-    :full_name,
-    :wholesale_price,
-    :retail_price
-  )
+  Product = KeywordStruct.new(:id, :full_name, :wholesale_price, :retail_price) do
+    def brewery
+      full_name.match(/(.+?)\s+-/)[1]
+    end
+
+    def name
+      title_substring.match(/-\s+(.+)/)[1]
+    end
+
+    def style
+      options_substring.match(/(.+?)\./)[1].strip
+    end
+
+    def to_row
+      [brewery, name, style]
+    end
+
+    private
+
+    def options_substring
+      full_name[full_name.rindex('(')+1...full_name.size-1]
+    end
+
+    def title_substring
+      full_name[0...full_name.rindex('(')-1]
+    end
+  end
 end
